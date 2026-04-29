b12d7a26b8ed434cd9c9c071384fdac786b7c631
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2026-04-28 23:36:53 -0600

    "ui fix"

--- MtdrSpring/backend/src/main/frontend/src/task/LogActualHoursModal.js
+++ MtdrSpring/backend/src/main/frontend/src/task/LogActualHoursModal.js
@@ -52,7 +52,7 @@ function LogActualHoursModal({
           </p>
 
           <div className="TM-field">
-            <label className="TM-label">Actual Hours TU TIO2</label>
+            <label className="TM-label">Actual Hours</label>
             <input
               className="TM-input"
               type="number"
